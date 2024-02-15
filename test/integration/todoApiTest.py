56923a5acd168a8e1e4d08a89c46329e68fc91f0
XabiLR <152807524+XabiLR@users.noreply.github.com>
2024-02-15 19:24:22 +0100

    Update todoApiTest.py

--- test/integration/todoApiTest.py
+++ test/integration/todoApiTest.py
@@ -7,8 +7,8 @@ import json
 
 import pytest
 
-#BASE_URL = os.environ.get("BASE_URL")
-BASE_URL = "https://536eojxwxc.execute-api.us-east-1.amazonaws.com/Prod"
+BASE_URL = os.environ.get("BASE_URL")
+#BASE_URL = "https://536eojxwxc.execute-api.us-east-1.amazonaws.com/Prod"
 DEFAULT_TIMEOUT = 2  # in secs
 
 
